d28d29e64497119a5e9b55630741ebb5968ade9e
Laercio Nascimento <laercio@protonmail.ch>
2021-08-12 08:08:50 -0300

    load fonts and move StatusBar

--- App.tsx
+++ App.tsx
@@ -1,6 +1,35 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from '@expo-google-fonts/rajdhani';
+
 import { SignInScreen } from './src/screens/SignInScreen';
 
 export default function App() {
-  return <SignInScreen />;
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <SignInScreen />
+    </>
+  );
 }
