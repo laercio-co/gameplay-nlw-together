d39b40a1a871bf7714b4dacd6db637c1190b7f13
Laercio Nascimento <laercio@protonmail.ch>
2021-08-12 08:03:06 -0300

    Improvements

--- src/screens/SignInScreen/index.tsx
+++ src/screens/SignInScreen/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, ScrollView, StatusBar, Text, View } from 'react-native';
+import { Image, ScrollView, Text, View } from 'react-native';
 
 import IllustrationImg from '../../assets/illustration.png';
 import { ButtonIcon } from '../../components/ButtonIcon';
@@ -9,12 +9,6 @@ export function SignInScreen() {
   return (
     <ScrollView>
       <View style={styles.container}>
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor="transparent"
-          translucent
-        />
-
         <Image
           style={styles.image}
           source={IllustrationImg}
@@ -23,9 +17,8 @@ export function SignInScreen() {
 
         <View style={styles.content}>
           <Text style={styles.title}>
-            Organize {'\n'}
-            sua jogatinas {'\n'}
-            facilmente
+            Conecte-se {'\n'}e organize suas {'\n'}
+            jogatinas
           </Text>
 
           <Text style={styles.subtitle}>
