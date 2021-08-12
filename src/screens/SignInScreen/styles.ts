1abe4edb7e7bbd1338ce2858e76286d926c9ea0d
Laercio Nascimento <laercio@protonmail.ch>
2021-08-12 08:04:04 -0300

    add new properties to styles

--- src/screens/SignInScreen/styles.ts
+++ src/screens/SignInScreen/styles.ts
@@ -7,7 +7,7 @@ export const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: theme.colors.secondary90,
+    backgroundColor: theme.colors.secondary100,
   },
   image: {
     width: '100%',
@@ -23,11 +23,15 @@ export const styles = StyleSheet.create({
     fontSize: 40,
     textAlign: 'center',
     marginBottom: 16,
+    fontFamily: theme.fonts.title700,
+    lineHeight: 40,
   },
   subtitle: {
     color: theme.colors.heading,
     fontSize: 15,
     textAlign: 'center',
     marginBottom: 64,
+    fontFamily: theme.fonts.title500,
+    lineHeight: 25,
   },
 });
