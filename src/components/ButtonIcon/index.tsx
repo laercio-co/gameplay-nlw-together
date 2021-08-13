48e93f346c6e87c4d351d9302bc3e7c8da3a22a4
Laercio Nascimento <laercio@protonmail.ch>
2021-08-13 16:01:30 -0300

    change TouchableOpacity from ButtonIcon

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import {
-  Image,
-  Text,
-  TouchableOpacity,
-  TouchableOpacityProps,
-  View,
-} from 'react-native';
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
+import { Image, Text, View } from 'react-native';
 
 import DiscordImg from '../../assets/discord.png';
 import { styles } from './styles';
 
-type ButtonIconProps = TouchableOpacityProps & {
+type ButtonIconProps = RectButtonProps & {
   title: string;
 };
 
@@ -18,12 +13,12 @@ export function ButtonIcon(props: ButtonIconProps) {
   const { title, ...rest } = props;
 
   return (
-    <TouchableOpacity style={styles.container} {...rest}>
+    <RectButton style={styles.container} {...rest}>
       <View style={styles.iconWrapper}>
         <Image source={DiscordImg} style={styles.icon} />
       </View>
 
       <Text style={styles.title}>{title}</Text>
-    </TouchableOpacity>
+    </RectButton>
   );
 }
